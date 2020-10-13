73b7fc9809d06679dc4d5f7da24cf17cdafad3f2
Frank van der Linden <fllinden@amazon.com>
2020-10-13 22:24:46 +0000

    lustre: Fix compilation with MOFED 5.1
        LU-13761 o2ib: Fix compilation with MOFED 5.1
    
        A new argument was added to rdma_reject() in MOFED 5.1 and
        Linux 5.8.
    
        Add a cofigure check and support both versions of rdma_reject().
    
        Test-Parameters: trivial
        Signed-off-by: Sergey Gorenko <sergeygo@mellanox.com>
        Change-Id: I2b28991f335658b651b21a09899b7b17ab2a9d57
        Reviewed-on: https://review.whamcloud.com/39323
        Reviewed-by: Neil Brown <neilb@suse.de>
        Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
        Reviewed-by: James Simmons <jsimmons@infradead.org>
        Tested-by: jenkins <devops@whamcloud.com>
        Tested-by: Maloo <maloo@whamcloud.com>
        Reviewed-by: Oleg Drokin <green@whamcloud.com>
    
    Signed-off-by: Frank van der Linden <fllinden@amazon.com>

--- drivers/staging/lustrefsx/lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ drivers/staging/lustrefsx/lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -2217,7 +2217,11 @@ kiblnd_reject(struct rdma_cm_id *cmid, kib_rej_t *rej)
 {
         int          rc;
 
+#ifdef HAVE_RDMA_REJECT_4ARGS
+	rc = rdma_reject(cmid, rej, sizeof(*rej), IB_CM_REJ_CONSUMER_DEFINED);
+#else
         rc = rdma_reject(cmid, rej, sizeof(*rej));
+#endif
 
         if (rc != 0)
                 CWARN("Error %d sending reject\n", rc);
