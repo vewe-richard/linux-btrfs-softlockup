a70e0d058c5ecf1baa4da3a9ef687a8197ed0b9b
KP Singh <kpsingh@google.com>
2020-11-06 10:37:40 +0000

    bpf: Implement task local storage
    commit 4cf1bc1f10452065a29d576fc5693fc4fab5b919 upstream.
    
    Similar to bpf_local_storage for sockets and inodes add local storage
    for task_struct.
    
    The life-cycle of storage is managed with the life-cycle of the
    task_struct.  i.e. the storage is destroyed along with the owning task
    with a callback to the bpf_task_storage_free from the task_free LSM
    hook.
    
    The BPF LSM allocates an __rcu pointer to the bpf_local_storage in
    the security blob which are now stackable and can co-exist with other
    LSMs.
    
    The userspace map operations can be done by using a pid fd as a key
    passed to the lookup, update and delete operations.
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20201106103747.2780972-3-kpsingh@chromium.org
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -4951,6 +4951,11 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 		    func_id != BPF_FUNC_inode_storage_delete)
 			goto error;
 		break;
+	case BPF_MAP_TYPE_TASK_STORAGE:
+		if (func_id != BPF_FUNC_task_storage_get &&
+		    func_id != BPF_FUNC_task_storage_delete)
+			goto error;
+		break;
 	default:
 		break;
 	}
@@ -5033,6 +5038,11 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 	case BPF_FUNC_inode_storage_get:
 	case BPF_FUNC_inode_storage_delete:
 		if (map->map_type != BPF_MAP_TYPE_INODE_STORAGE)
+			goto error;
+		break;
+	case BPF_FUNC_task_storage_get:
+	case BPF_FUNC_task_storage_delete:
+		if (map->map_type != BPF_MAP_TYPE_TASK_STORAGE)
 			goto error;
 		break;
 	default:
