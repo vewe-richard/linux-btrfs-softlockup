38be96aee9137f1a99be3d12658c1d0490c718f9
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2021-05-26 16:49:17 -0500

    arm64: Create a list of SYM_CODE functions, check return PC against list
    The unwinder should check if the return PC falls in any function that
    is considered unreliable from an unwinding perspective. If it does,
    mark the stack trace unreliable.
    
    Function types
    ==============
    
    The compiler generates code for C functions and assigns the type STT_FUNC
    to them.
    
    Assembly functions are manually assigned a type:
    
    	- STT_FUNC for functions defined with SYM_FUNC*() macros
    
    	- STT_NONE for functions defined with SYM_CODE*() macros
    
    In the future, STT_FUNC functions will be analyzed by objtool and "fixed"
    as necessary. So, they are not "interesting" to the reliable unwinder in
    the kernel.
    
    That leaves SYM_CODE*() functions. These contain low-level code that is
    difficult or impossible for objtool to analyze. So, objtool ignores them
    leaving them to the reliable unwinder. These functions must be considered
    unreliable from an unwinding perspective.
    
    Define a special section for unreliable functions
    =================================================
    
    Define a SYM_CODE_END() macro for arm64 that adds the function address
    range to a new section called "sym_code_functions".
    
    Linker file
    ===========
    
    Include the "sym_code_functions" section under initdata in vmlinux.lds.S.
    
    Initialization
    ==============
    
    Define an early_initcall() to copy the function address ranges from the
    "sym_code_functions" section to an array by the same name.
    
    Unwinder check
    ==============
    
    Define a function called unwinder_is_unreliable() that compares a return
    PC with sym_code_functions[]. If there is a match, then mark the stack trace
    as unreliable. Call unwinder_is_unreliable() from unwind_frame().
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
    [Move final frame check; if a SYM_CODE function occurs in the very last frame in
     the stack trace then it is not considered unreliable because there is no more
     unwinding to do]
    Signed-off-by: Suraj Jitindar Singh <surajjs@amazon.com>

--- arch/arm64/kernel/stacktrace.c
+++ arch/arm64/kernel/stacktrace.c
@@ -12,11 +12,115 @@
 #include <linux/sched/debug.h>
 #include <linux/sched/task_stack.h>
 #include <linux/stacktrace.h>
+#include <linux/slab.h>
 
 #include <asm/irq.h>
 #include <asm/pointer_auth.h>
 #include <asm/stack_pointer.h>
 #include <asm/stacktrace.h>
+
+struct code_range {
+	unsigned long	start;
+	unsigned long	end;
+};
+
+static struct code_range	*sym_code_functions;
+static int			num_sym_code_functions;
+
+int __init init_sym_code_functions(void)
+{
+	size_t size;
+
+	size = (unsigned long)__sym_code_functions_end -
+	       (unsigned long)__sym_code_functions_start;
+
+	sym_code_functions = kmalloc(size, GFP_KERNEL);
+	if (!sym_code_functions)
+		return -ENOMEM;
+
+	memcpy(sym_code_functions, __sym_code_functions_start, size);
+	/* Update num_sym_code_functions after copying sym_code_functions. */
+	smp_mb();
+	num_sym_code_functions = size / sizeof(struct code_range);
+
+	return 0;
+}
+early_initcall(init_sym_code_functions);
+
+/*
+ * Check the return PC against sym_code_functions[]. If there is a match, then
+ * the consider the stack frame unreliable. These functions contain low-level
+ * code where the frame pointer and/or the return address register cannot be
+ * relied upon. This addresses the following situations:
+ *
+ *	- Exception handlers and entry assembly
+ *	- Trampoline assembly (e.g., ftrace, kprobes)
+ *	- Hypervisor-related assembly
+ *	- Hibernation-related assembly
+ *	- CPU start-stop, suspend-resume assembly
+ *	- Kernel relocation assembly
+ *
+ * Some special cases covered by sym_code_functions[] deserve a mention here:
+ *
+ *	- All EL1 interrupt and exception stack traces will be considered
+ *	  unreliable. This is the correct behavior as interrupts and exceptions
+ *	  can happen on any instruction including ones in the frame pointer
+ *	  prolog and epilog. Unless stack metadata is available so the unwinder
+ *	  can unwind through these special cases, such stack traces will be
+ *	  considered unreliable.
+ *
+ *	- A task can get preempted at the end of an interrupt. Stack traces
+ *	  of preempted tasks will show the interrupt frame in the stack trace
+ *	  and will be considered unreliable.
+ *
+ *	- Breakpoints are exceptions. So, all stack traces in the break point
+ *	  handler (including probes) will be considered unreliable.
+ *
+ *	- All of the ftrace entry trampolines are considered unreliable. So,
+ *	  all stack traces taken from tracer functions will be considered
+ *	  unreliable.
+ *
+ *	- The Function Graph Tracer return trampoline (return_to_handler)
+ *	  and the Kretprobe return trampoline (kretprobe_trampoline) are
+ *	  also considered unreliable.
+ *
+ * Some of the special cases above can be unwound through using special logic
+ * in unwind_frame().
+ *
+ *	- return_to_handler() is handled by the unwinder by attempting to
+ *	  retrieve the original return address from the per-task return
+ *	  address stack.
+ *
+ *	- kretprobe_trampoline() can be handled in a similar fashion by
+ *	  attempting to retrieve the original return address from the per-task
+ *	  kretprobe instance list.
+ *
+ *	- I reckon optprobes can be handled in a similar fashion in the future?
+ *
+ *	- Stack traces taken from the FTrace tracer functions can be handled
+ *	  as well. ftrace_call is an inner label defined in the Ftrace entry
+ *	  trampoline. This is the location where the call to a tracer function
+ *	  is patched. So, if the return PC equals ftrace_call+4, it is
+ *	  reliable. At that point, proper stack frames have already been set
+ *	  up for the traced function and its caller.
+ */
+static bool unwinder_is_unreliable(unsigned long pc)
+{
+	const struct code_range *range;
+	int i;
+
+	/*
+	 * If sym_code_functions[] were sorted, a binary search could be
+	 * done to make this more performant.
+	 */
+	for (i = 0; i < num_sym_code_functions; i++) {
+		range = &sym_code_functions[i];
+		if (pc >= range->start && pc < range->end)
+			return true;
+	}
+
+	return false;
+}
 
 /*
  * AArch64 PCS assigns the frame pointer to x29.
@@ -48,10 +152,6 @@ int notrace unwind_frame(struct task_struct *tsk, struct stackframe *frame)
 
 	if (!tsk)
 		tsk = current;
-
-	/* Final frame; nothing to unwind */
-	if (fp == (unsigned long)task_pt_regs(tsk)->stackframe)
-		return -ENOENT;
 
 	if (fp & 0xf) {
 		frame->reliable = false;
@@ -133,7 +233,24 @@ int notrace unwind_frame(struct task_struct *tsk, struct stackframe *frame)
 	 *	- Foreign code (e.g. EFI runtime services)
 	 *	- Procedure Linkage Table (PLT) entries and veneer functions
 	 */
-	if (!__kernel_text_address(frame->pc))
+	if (!__kernel_text_address(frame->pc)) {
+		frame->reliable = false;
+		return 0;
+	}
+
+	/*
+	 * If the final frame has been reached, there is no more unwinding
+	 * to do. There is no need to check if the return PC is considered
+	 * unreliable by the unwinder.
+	 */
+	if (!frame->fp)
+		return 0;
+
+	/* Final frame; nothing to unwind */
+	if (frame->fp == (unsigned long)task_pt_regs(tsk)->stackframe)
+		return -ENOENT;
+
+	if (unwinder_is_unreliable(frame->pc))
 		frame->reliable = false;
 
 	return 0;
